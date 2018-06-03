8417d51b03af9424d8ef39e9ada8e08750f196f3
MB4HP0M4DE <39848788+MBAHPOMADE@users.noreply.github.com>
2018-06-03 06:34:41 -0700

    setup: add setup.sh for build with GCC

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#export CROSS_COMPILE=$HOME/gcc49/bin/aarch64-linux-android-
+export CROSS_COMPILE=aarch64-linux-gnu-
+export ARCH=arm64
+
+export KBUILD_BUILD_USER=MBAHPOMADE
+export KBUILD_BUILD_HOST=kernel
+
+jobs=10
+
+export CFLAGS=""
+export CXXFLAGS=""
+export LDFLAGS=""
+
+unalias cat > /dev/null 2>&1
+unalias zip > /dev/null 2>&1
+
+cc_ver="$(${CROSS_COMPILE}gcc --version|head -n1|cut -d'(' -f2|tr -d ')'|awk '{$5=""; print $0}'|sed -e 's/[[:space:]]*$//')"
+MAKEFLAGS=("KBUILD_COMPILER_STRING=${cc_ver}")
+
+# helpers
+source helpers.sh
